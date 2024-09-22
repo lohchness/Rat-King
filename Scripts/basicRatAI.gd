ffb50ccfe25890a7ee5e061df6a306523b6029f9
Lok/Tom Chow <lohchness@gmail.com>
2024-09-22 18:40:00 +1000

    Reparenting working

--- Scripts/basicRatAI.gd
+++ Scripts/basicRatAI.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
+class_name BasicRat
 
 @onready var stateChart: StateChart = get_node("StateChart")
 var rng = RandomNumberGenerator.new()
 
+@onready var physicsCollider: CollisionPolygon2D = $Collision
+@onready var grabOthersArea: Area2D = $GrabOtherRadius
+@onready var getTakenArea: Area2D = $GetTakenRadius
+
+## By default this is a solo enemy rat.
+## Default groups and settings are directly set in the editor.
 func _ready() -> void:
 	pass
 
 #################### SOLO ##############################
 
+## GetTakenRadius is enabled only when solo.
+
+var direction: Vector2
 var speed = 100
 var accel = 10
 
@@ -17,54 +27,72 @@ var near_player = false
 
 func _on_solo_state_entered() -> void:
 	WanderTimer.start()
-	change_direction()	
+	change_direction() ## RANDOM MOVEMENT
+	
+	getTakenArea.add_to_group("EnemyRatSolo")
+	getTakenArea.monitorable = true
 
 func _on_solo_state_processing(delta: float) -> void:
 	outline.visible = near_player
 
 func _on_solo_state_physics_processing(delta: float) -> void:
-	rotation = lerp_angle(rotation, velocity.angle(), 20 * delta)
+	return
+	rotation = lerp_angle(rotation, velocity.angle(), 10 * delta)
+	velocity = direction * speed
 	move_and_slide()
 
 func _on_solo_state_exited() -> void:
 	WanderTimer.stop()
 	outline.visible = false
+	
+	getTakenArea.remove_from_group("EnemyRatSolo")
+	getTakenArea.monitorable = false
 
 func change_direction():
 	var angle = rng.randf_range(0, 2 * PI)
-	velocity = Vector2(cos(angle), sin(angle)) * speed
+	direction = Vector2(cos(angle), sin(angle))
 
 func _on_timer_timeout() -> void:
 	change_direction()
 
-## If a player's rat is near an enemy rat
-func _on_get_taken_radius_area_entered(area: Area2D) -> void:
-	if area.is_in_group("PlayerRat"):
-		near_player = true
+## An enemy Rat will be near a player if the GetTakenRadius is 
+## colliding with PlayerRat's GrabOtherRadius.
+## self.GetTakenRadius   EnemyRatSolo
+## area                  PlayerRat
 
-func _on_get_taken_radius_area_exited(area: Area2D) -> void:
-	if area.is_in_group("PlayerRat"):
-		near_player = false
+func entered_player_grab_radius():
+	near_player = true
+
+func exited_player_grab_radius():
+	near_player = false
+
+#func _on_get_taken_radius_area_entered(area: Area2D) -> void:
+	#if area.is_in_group("PlayerRat"):
+		#near_player = true
+#
+#func _on_get_taken_radius_area_exited(area: Area2D) -> void:
+	#if area.is_in_group("PlayerRat"):
+		#near_player = false
 
 #################### PACK (PLAYER) ##############################
 
 ## Rats controlled by the player. Here, all movement and physics is handled by
-## the RatPack class. This Rat's Collision polygon will be reparented to RatPack
-## and will be deleted here.
+## the RatPack class. As far as this BasicRat is concerned, they are being
+## controlled by an outside entity - movement, position, appearance, etc.
 
+## RatPack will handle reparenting the following nodes:
+## - This Rat's Physics CollisionPolygon2D
+## - This Rat's GrabOtherRadius 
+
+## GrabOtherRadius is enabled only when in a pack.
+
+## Which Pack this Rat belongs to
 var pack: RatPack
 
-func add_to_pack(r: RatPack):
-	pack = r
+func add_to_pack(body: RatPack):
+	pack = body
 	stateChart.send_event("added_to_pack")
-	
-	## 
-	$GrabOtherRadius.add_to_group("PlayerRat")
 
 func _on_pack_state_entered() -> void:
-	pass
-
-## Polling
-func _on_pack_state_input(event: InputEvent) -> void:
-	
-	pass # Replace with function body.
+	grabOthersArea.add_to_group("PlayerRat")
+	grabOthersArea.monitoring = true
--- Scripts/ratPack.gd
+++ Scripts/ratPack.gd
@@ -6,14 +6,17 @@ const BasicRat = preload("res://Scenes/BasicRat.tscn")
 
 var base_speed = 500
 var base_accel = 50
-
 var curr_speed
 var curr_accel
-
 var is_moving : bool
 
+@onready var grabOthersAreas = $GrabOthers
 var num_rats = 0
-var last_rat_added
+var last_rat_added: BasicRat
+
+var rat_bodies
+var rat_colliders
+
 
 func _ready() -> void:
 	curr_speed = base_speed
@@ -21,21 +24,17 @@ func _ready() -> void:
 	
 	## Add initial rat
 	var firstrat = BasicRat.instantiate()
-	get_node("Rats").add_child(firstrat)
-	
-	last_rat_added = firstrat
-	num_rats += 1
+	get_node("BasicRatBodies").add_child(firstrat)
 	
 	firstrat.call_deferred("add_to_pack", self) ## Wait for tree to finish initializing
-	
-	## Change collision parent
-	var collision_node = firstrat.get_node("Collision")
-	firstrat.remove_child(collision_node)
-	add_child(collision_node)
+	add_rat_to_this_pack(firstrat)
 
 func _physics_process(delta: float) -> void:
+	
 	if num_rats == 1:
 		last_rat_added.position = last_rat_added.get_node("Center").position
+		last_rat_added.physicsCollider.position = last_rat_added.get_node("Center").position
+	
 	
 	## Movement
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -46,4 +45,44 @@ func _physics_process(delta: float) -> void:
 	## Rotate based on direction
 	is_moving = velocity.length() > 0.01
 	if is_moving:
-		rotation = lerp_angle(rotation, velocity.angle(), 20 * delta)
+		rotation = lerp_angle(rotation, velocity.angle(), 10 * delta)
+	
+	
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("space"):
+		var bodies: Array = grabOthersAreas.get_overlapping_areas()\
+			.filter(func (x): return x.is_in_group("EnemyRatSolo"))\
+			.map(func (x): return x.get_parent())
+		
+		if (len(bodies)):
+			var new_rat = bodies.pick_random()
+			add_rat_to_this_pack(new_rat)
+
+## Reparents the collision nodes of a BasicRat
+func add_rat_to_this_pack(body: BasicRat):
+	body.add_to_pack(self)
+	
+	## APPEARANCE
+	
+	## COLLISION AND AREAS
+	
+	var collision: CollisionPolygon2D = body.get_node("Collision")
+	collision.reparent(self)
+	
+	## Get CollisionShape of GrabOthersRadius
+	## This will be put into GrabOthers Area2D to detect other rats to grab.
+	var grab_area = body.get_node("GrabOtherRadius").get_child(0)
+	grab_area.reparent(grabOthersAreas)
+	
+	last_rat_added = body
+	num_rats += 1
+
+## Called when GrabOthers collides with another Area.
+func _on_grab_others_area_entered(area: Area2D) -> void:
+	if area.is_in_group("EnemyRatSolo"):
+		area.get_parent().call("entered_player_grab_radius")
+
+func _on_grab_others_area_exited(area: Area2D) -> void:
+	if area.is_in_group("EnemyRatSolo"):
+		area.get_parent().call("exited_player_grab_radius")
